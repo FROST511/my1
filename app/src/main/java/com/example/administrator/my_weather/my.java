b01f2f0dfd7dacff795b226f1f8103a89e5b0e98
FROST511 <1242618848@qq.com>
2016-05-24 13:38:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/my_weather/my.java
@@ -0,0 +1,13 @@
+package com.example.administrator.my_weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class my extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
